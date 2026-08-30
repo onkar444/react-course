597b601f5ea7d8b04c3f4f7f94fda66385d1a480
onkar raut <onkarraut97@gmail.com>
2026-08-30 12:07:54 +0530

    Migrating our chatbot.html to React setup

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,122 +1,133 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import { useEffect, useRef, useState } from 'react'
+import { Chatbot } from 'supersimpledev'
 import './App.css'
+import RobotProfileImage from './assets/robot.png'
+import UserProfileImage from './assets/user.png'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('');
+
+
+  function saveInputText(e) {
+    setInputText(e.target.value);
+  }
+
+  function sendMessage() {
+
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      }
+    ];
+
+    setChatMessages(newChatMessages);
+
+    const response = Chatbot.getResponse(inputText);
+    setChatMessages([
+      ...newChatMessages, {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ])
+
+    setInputText('');
+  }
+  return (
+    <div className="chat-input-container">
+      <input
+        className="chat-input"
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        value={inputText} />
+      <button className="send-button" onClick={sendMessage}>Send</button>
+    </div>
+  )
+}
+
+function ChatMessage({ message, sender }) {
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className={
+      sender === 'user'
+        ? 'chat-message-user'
+        : 'chat-message-robot'}>
+      {sender === "robot" && <img src={RobotProfileImage} className="chat-message-profile" />}
+      <div className="chat-message-text">
+        {message}
+      </div>
+      {sender === "user" && <img src={UserProfileImage} className="chat-message-profile" />}
+    </div>
+  );
+}
 
-      <div className="ticks"></div>
+function ChatMessages({ chatMessages }) {
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+  const chatMessagesRef = useRef(null);
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id} />
+        )
+      })}
+    </div>
+  );
+}
+
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([
+    {
+      message: "hello chatbot",
+      sender: "user",
+      id: 'id1'
+    },
+    {
+      message: "Hello! How can I help you?",
+      sender: "robot",
+      id: 'id2'
+    },
+    {
+      message: "can you get me today's date?",
+      sender: "user",
+      id: 'id3'
+    },
+    {
+      message: "Today's date is September 21",
+      sender: "robot",
+      id: 'id4'
+    }
+  ]);
+
+  return (
+    <div className="app-container">
+
+      <ChatMessages
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages} />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages} />
+    </div>
+  );
 }
 
 export default App
