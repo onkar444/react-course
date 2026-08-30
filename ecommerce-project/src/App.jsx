7c60b3183d8e5e1bdf75045b8107298ce21c230c
onkar444 <onkarraut97@gmail.com>
2026-08-30 15:40:34 +0530

    Move orders page and tracking page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,12 +2,16 @@ import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
 import './App.css'
 import { Route, Routes } from 'react-router'
+import { OrderPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path='/checkout' element={<CheckoutPage />} />
+      <Route path='/orders' element={<OrderPage />} />
+      <Route path='/tracking' element={<TrackingPage />} />
     </Routes>
   )
 }
